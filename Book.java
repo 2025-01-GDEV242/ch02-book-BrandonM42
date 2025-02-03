3ec081c68c05ee4b2d4a5849bc58a152d9daa0eb
BrandonM42 <g00320832@stu.raritanval.edu>
2025-02-03 11:20:41 -0500

    steps 84 and 85

--- Book.java
+++ Book.java
@@ -11,16 +11,35 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Add the methods here ...
+    
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getPages()
+    {
+        return pages;
+    }
+    
+    
 }
